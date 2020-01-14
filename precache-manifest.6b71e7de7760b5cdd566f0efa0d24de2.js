77ba55b9f86201fdbb72eec95fc7771e2df5c5c8
AlexHodovas <o.hodovas@gmail.com>
2020-01-14 20:24:11 +0200

    Updates

---
+++ precache-manifest.6b71e7de7760b5cdd566f0efa0d24de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bf1b488b7b822d73f09d6cc8c0300f",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "9ba9ff1935213ec2105e",
+    "url": "/react_uber-eats/static/css/main.90231507.chunk.css"
+  },
+  {
+    "revision": "b780e2f570c2309f1dee",
+    "url": "/react_uber-eats/static/js/2.8dc4d5f9.chunk.js"
+  },
+  {
+    "revision": "9ba9ff1935213ec2105e",
+    "url": "/react_uber-eats/static/js/main.69a06b5c.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
