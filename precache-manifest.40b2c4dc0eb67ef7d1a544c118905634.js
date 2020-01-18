839aec4a116a84b4171a871ed0d4f220e5923e72
AlexHodovas <o.hodovas@gmail.com>
2020-01-18 17:31:42 +0200

    Updates

---
+++ precache-manifest.40b2c4dc0eb67ef7d1a544c118905634.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a181bc1675098fe22af892f532b85e11",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "f8001e24415643bad3b7",
+    "url": "/react_uber-eats/static/css/main.3cad0474.chunk.css"
+  },
+  {
+    "revision": "7b6e3bd3c5d7c0f403fa",
+    "url": "/react_uber-eats/static/js/2.80f006aa.chunk.js"
+  },
+  {
+    "revision": "f8001e24415643bad3b7",
+    "url": "/react_uber-eats/static/js/main.902310f5.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.6b71e7de7760b5cdd566f0efa0d24de2.js"
+  "/react_uber-eats/precache-manifest.40b2c4dc0eb67ef7d1a544c118905634.js"
 );
 
 self.addEventListener('message', (event) => {
